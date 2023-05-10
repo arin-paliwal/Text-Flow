cd2061ccdae622ceff25903c351f8a41eb6bd45f
Arin Paliwal <paliwalarin09@gmail.com>
2023-05-10 14:16:56 +0530

    Added Deployment Variables

--- backend/server.js
+++ backend/server.js
@@ -6,18 +6,34 @@ const { chats } = require("./data/data");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require('./routes/chatRoutes');
 const messageRoutes = require('./routes/messageRoutes');
-
+const path = require("path");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 dotenv.config();
 connectDB();
 const app = express()
 app.use(express.json()); // to accept json data
-app.get('/', (req, res) => {
-    res.send("API is running successfully");
-});
+
 app.use('/api/user', userRoutes);
 app.use('/api/chat', chatRoutes);
 app.use('/api/message', messageRoutes)
+
+
+// ---- Deployment
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname1, "/frontend/build")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname1,"frontend","build","index.html"));
+    });
+}
+else {
+    app.get('/', (req, res) => {
+        res.send("API is running successfully");
+    });
+}
+// ---- Deployment
+
+
 app.use(notFound);
 app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
@@ -44,14 +60,14 @@ io.on("connection", (socket) => {
     socket.on("new message", (newMessageRecieved) => {
         // console.log("newMessageRecieved"+newMessageRecieved);
         var chat = newMessageRecieved.chat;
-        console.log("logging the chat here", chat);
+        // console.log("logging the chat here", chat);
         if (!chat.users) return console.log("not defined");
         chat.users.forEach((user) => {
             if (user._id == newMessageRecieved.sender._id) return;
             socket.in(user._id).emit("message recieved", newMessageRecieved);
         });
     });
-    socket.off("setup",()=>{
+    socket.off("setup", () => {
         console.log("User Disconnected");
         socket.leave(userData._id);
     });
